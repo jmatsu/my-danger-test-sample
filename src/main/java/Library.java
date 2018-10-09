d35cb7458d754e371bbc5ed9c7c98607a6bbe3aa
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-10-09 22:53:26 +0900

    Added wrong methods

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,4 +5,12 @@ public class Library {
     public boolean someLibraryMethod() {
         return true;
     }
+
+    public void snake_case_method() {
+        System.out.println("Snake case");
+    }
+
+    public void wrongIndent() {
+            // space must be 4
+    }
 }
